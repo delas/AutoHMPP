c3a60c34a3682859efd67d078cf0009fa567a167
delas <delas@a6f00bd5-ff82-0410-82e6-d0698ff6f273>
2011-09-27 15:48:58 +0000

    - Let the old algorithm run

--- sw/AutoHMPP/trunk/src/it/processmining/autohmpp/utils/Utils.java
+++ sw/AutoHMPP/trunk/src/it/processmining/autohmpp/utils/Utils.java
@@ -1,5 +1,12 @@
 package it.processmining.autohmpp.utils;
 
+import java.io.IOException;
+
+import org.processmining.framework.log.LogFile;
+import org.processmining.framework.log.LogFilter;
+import org.processmining.framework.log.LogReader;
+import org.processmining.framework.log.LogReaderFactory;
+import org.processmining.framework.log.filter.DefaultLogFilter;
 import org.processmining.framework.models.heuristics.HNSubSet;
 import org.processmining.framework.models.heuristics.HeuristicsNet;
 
@@ -21,5 +28,50 @@ public class Utils {
 			connections += set.size();
 		}
 		return connections;
+	}
+	
+	
+	/**
+	 * This method loads a log file into a logfile object 
+	 * 
+	 * @param inputFile
+	 * @return
+	 */
+	public static LogReader loadLog(String inputFile) {
+		LogFile file;
+		LogFilter filter = null;
+
+		try {
+			if (inputFile.endsWith(".zip")) {
+				file = LogFile.getInstance("zip://" + inputFile
+						+ "#TestProcess.mxml");
+			} else {
+				file = LogFile.getInstance(inputFile);
+			}
+			file.getInputStream();
+		} catch (Exception e) {
+			file = null;
+		}
+		if (file == null) {
+			try {
+				file = LogFile.instantiateEmptyLogFile(inputFile);
+				filter = new DefaultLogFilter(DefaultLogFilter.INCLUDE);
+			} catch (IOException ex3) {
+				System.out
+						.println("Cannot create empty Log file: " + inputFile);
+				file = null;
+				filter = null;
+			}
+		}
+
+		LogReader log = null;
+		try {
+			if (file != null) {
+				log = LogReaderFactory.createInstance(filter, file);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return log;
 	}
 }
